797c476df8100d413fd4032026ef489f073af549
wm <528980628@qq.com>
2022-08-24 10:40:11 +0800

    test1

--- mall-demo/src/main/java/com/macro/mall/demo/MallDemoApplication.java
+++ mall-demo/src/main/java/com/macro/mall/demo/MallDemoApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MallDemoApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(MallDemoApplication.class, args);
+		System.out.println(123);
+		System.out.println("test1");
 	}
 }
