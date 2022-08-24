d5a385d8e8f27fb8d3ae63f5a189cdcf770a8483
wm <528980628@qq.com>
2022-08-24 14:16:22 +0800

    test1

--- mall-demo/src/main/java/com/macro/mall/demo/MallDemoApplication.java
+++ mall-demo/src/main/java/com/macro/mall/demo/MallDemoApplication.java
@@ -8,5 +8,6 @@ public class MallDemoApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(MallDemoApplication.class, args);
 		System.out.println(123);
+		System.out.println("test1");
 	}
 }
